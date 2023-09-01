8f9156c34fa8fa66487432e3c15a25d8260e4f2a
CipherSharma <tudsharma@gmail.com>
2023-09-01 22:23:29 +0530

    test

--- Elastic_Search_Task/settings.py
+++ Elastic_Search_Task/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sdihlwzvu5rde582=im+2(9+&7z_$s&m!_*2@$mvryfi-yvq#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 
 ALLOWED_HOSTS = ["*"]
 
